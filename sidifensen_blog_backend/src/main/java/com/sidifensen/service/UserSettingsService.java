9c10ededee2155cdfd6a502eb0ec4bf369fa2cb9
sidifensen <1848221808@qq.com>
2026-03-19 10:55:22 +0800

    feat: 添加 createDefaultSettings 接口方法

--- sidifensen_blog_backend/src/main/java/com/sidifensen/service/UserSettingsService.java
+++ sidifensen_blog_backend/src/main/java/com/sidifensen/service/UserSettingsService.java
@@ -22,6 +22,14 @@ public interface UserSettingsService extends IService<UserSettings> {
     UserSettings getSettings(Integer userId);
 
     /**
+     * 为新用户创建默认的个人设置
+     * 所有邮件通知默认关闭
+     *
+     * @param userId 用户 ID
+     */
+    void createDefaultSettings(Integer userId);
+
+    /**
      * 获取用户的私信邮件通知设置
      *
      * @param userId 用户 ID
@@ -36,5 +44,21 @@ public interface UserSettingsService extends IService<UserSettings> {
      * @param isReceive 是否接收（0-关闭，1-开启）
      */
     void setReceivePrivateMessageEmail(Integer userId, Integer isReceive);
+
+    /**
+     * 获取用户的评论邮件通知设置
+     *
+     * @param userId 用户 ID
+     * @return 是否接收（0-关闭，1-开启），如果没有记录则返回默认值 1（开启）
+     */
+    Integer getReceiveCommentEmail(Integer userId);
+
+    /**
+     * 获取用户的系统邮件通知设置
+     *
+     * @param userId 用户 ID
+     * @return 是否接收（0-关闭，1-开启），如果没有记录则返回默认值 1（开启）
+     */
+    Integer getReceiveSystemEmail(Integer userId);
 
 }
